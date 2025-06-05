460a0b091b0be167bfd4433dc4c5edf41197215a
PbSchnedR <pablo.schneider69@gmail.com>
2025-06-05 23:53:13 +0200

    maj name

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.get('/rss', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on https://rss-parser-lzjc.onrender.com`);
 });
